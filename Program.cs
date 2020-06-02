98ec70af1e007c5832fda2993c027e16a33035e9
dthompson999 <63125447+dthompson999@users.noreply.github.com>
2020-06-02 03:50:41 -0500

    Assignment Commit: Boxing Unboxing

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace boxing_unboxing
+{
+    class Program
+    {
+
+        static List<object> GetObjectList ()
+        {
+            return new List<object> ()
+            {
+                7,
+                28,
+                -1,
+                true,
+                "chair"
+            };
+        }
+
+        static void PrintObjectList (List<object> ol)
+        {
+            string s = "";
+            int num = 0;
+            for (int i = 0; i < ol.Count; i++)
+            {
+                if (ol[i] is int)
+                {
+                    num += (int) ol[i];
+                }
+                else if (ol[i] is string)
+                {
+                    s += ol[i] + " ";
+                }
+            }
+            Console.WriteLine($"the strings are:\n{s}");
+            Console.WriteLine($"the numbers are:\n{num}");
+        }
+
+        static void Main(string[] args)
+        {
+            PrintObjectList( GetObjectList () );
+        }
+    }
+}
